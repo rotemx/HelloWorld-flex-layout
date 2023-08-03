5a2335fd23bf68d084a95dc0121f3ca20e98da9a
Rotem Meiri <rotemx@users.noreply.github.com>
2023-08-03 20:30:35 +0300

    targil

--- Targil/targil1.js
+++ Targil/targil1.js
@@ -1,13 +1,41 @@
 let userInputParsed;
-
+let userInputStr
 do
 {
-	const
-		userInputStr = prompt('Enter a value:');
+	userInputStr = prompt('Enter a value:');
 	
 	userInputParsed = JSON.parse(userInputStr);
 	
+	const type = typeof userInputParsed;
+	
 	console.log('userInputParsed ', userInputParsed);
-	console.log('typeof userInputParsed', typeof userInputParsed);
+	console.log('typeof userInputParsed', type);
+
+	
+	if (userInputParsed === 7) {
+		console.log("Boom!");
+		break
+	}
+	const yesTypes = ['number', 'string', 'boolean'];
+	
+	let anyRuleApplied;
+	
+	if (yesTypes.includes(type))
+	{
+		anyRuleApplied = true;
+		console.log("Yes");
+	}
+	if (!userInputParsed)
+	{
+		anyRuleApplied = true;
+		console.log("False");
+	}
+	
+	if (!anyRuleApplied){
+		console.log('No');
+	}
 }
-while (userInputParsed !== 7)
+while (userInputParsed !== 7 && userInputStr !== null)
+
+
+// DRY - do not repeat yourself
