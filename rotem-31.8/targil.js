8c84784b7f7d0b8678dfd597532cf605b9fdae54
Rotem Meiri <rotemx@users.noreply.github.com>
2023-08-31 21:01:59 +0300

    added 24.8 - recursion start

--- rotem-31.8/targil.js
+++ rotem-31.8/targil.js
@@ -1,18 +1,17 @@
 const people = {
-	name: "John",
-	age: 59,
-	isMarried : true,
-	children : [{
-		name: "James",
-		age : 12,
-		children: []
-	},
+	name     : "John",
+	age      : 59,
+	isMarried: true,
+	children : [
 		{
-			name : "Jane",
-			age:45,
-			children: [{
-				// ..
-			}]
+			name    : "James",
+			age     : 12,
+			children: []
+		},
+		{
+			name    : "Jane",
+			age     : 45,
+			children: []
 		}]
 }
 
@@ -20,11 +19,27 @@ const people = {
 // (we don't know how many levels are there);
 
 //example :
-let requiredResult = [["John", 59],["James",12], ["Jane", 45]];
+let requiredResult = [["John", 59], ["James", 12], ["Jane", 45]];
 
 
+function extractNamesAndAgesFromObjects(obj)
+{
+	console.log(typeof obj, 'typeof obj');
+	if ((typeof obj !== "object") || obj === null || !(obj?.name) || !(obj?.age))
+	{
+		throw new Error('invalid people object');
+	}
+	
+	let result = [[obj.name, obj.age]];
+	
+	if (obj.children?.length)
+	{
+		result = [...result, ...obj.children?.map(child => extractNamesAndAgesFromObjects(child)).flat()];
+	}
+	return result
+}
 
-
+console.log(extractNamesAndAgesFromObjects(people));
 
 
 
